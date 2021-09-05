42caf0a3f85ee89d2c7631815799ecdc46619949
Александр Будко <budko-alex2014@yandex.ru>
2021-09-05 08:24:45 +0300

    Add project files.

---
+++ Task_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_1
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string path = "";
+            path = GetFilePath();            // Не знаю, где лежит файл, потому подумал будет логичнее сделать его выбор. 
+            
+
+            Console.ReadLine();
+        }
+
+        private static string  GetFilePath()
+        {
+            string s;
+            using (var ofDlg = new OpenFileDialog())
+            {
+                ofDlg.Filter = "Текстовые файлы (*.txt, *.csv)|*.txt";
+                ofDlg.CheckFileExists = true;
+                ofDlg.InitialDirectory = @"C:\";
+                if (ofDlg.ShowDialog() == DialogResult.OK)
+                {
+                    s = ofDlg.FileName;
+                }
+                else
+                {
+                    Console.WriteLine("Файл не выбран!");
+                    s = "";
+
+                }
+
+            }
+            return s;
+        }
+    }
+}
