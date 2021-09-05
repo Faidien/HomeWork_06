70b416b4e25e3e6b5d361edf09c35fdc1d734f9f
Александр Будко <budko-alex2014@yandex.ru>
2021-09-05 11:04:18 +0300

    Выбор файла, проверка валидности(число ли это, не выходит ли за границы

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using HomeWork_05;
+using System;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Task_1
@@ -13,32 +10,68 @@ namespace Task_1
         [STAThread]
         static void Main(string[] args)
         {
-            string path = "";
-            path = GetFilePath();            // Не знаю, где лежит файл, потому подумал будет логичнее сделать его выбор. 
-            
+            UtilityClass.PrintHeader("* * * * * Работа с числом N * * * * *");
+            if (CheckFile() > 0)
+            {
+
+            }
 
             Console.ReadLine();
         }
 
-        private static string  GetFilePath()
+        private static long CheckFile()
+        {
+            StreamReader reader;
+            string path = GetFilePath(out bool isFileChoose);
+            if (isFileChoose)
+            {
+                using (reader = new StreamReader(path))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        if (long.TryParse(reader.ReadLine(), out long N))
+                        {
+                            if (N < 1 || N > 1_000_000_000)
+                            {
+                                Console.WriteLine("Число выходит за пределы границ (1 - 1.000.000.000)!");
+                                return -1;
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Число N - {N}");
+                                return N;
+                            }
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Невозможно интерпертировать строку в файле как число!");
+                            return -1;
+                        }
+
+                    }
+                }
+                return -1;
+            }
+            else
+            {
+                Console.WriteLine("Файл не выбран!");
+                return -1;
+            }
+        }
+
+        // Не знаю, где лежит файл, потому подумал будет логичнее сделать его выбор. 
+        private static string GetFilePath(out bool isFileChoose)
         {
             string s;
             using (var ofDlg = new OpenFileDialog())
             {
-                ofDlg.Filter = "Текстовые файлы (*.txt, *.csv)|*.txt";
+                ofDlg.Filter = "Все файлы (*.*)|*.*";
                 ofDlg.CheckFileExists = true;
-                ofDlg.InitialDirectory = @"C:\";
-                if (ofDlg.ShowDialog() == DialogResult.OK)
-                {
-                    s = ofDlg.FileName;
-                }
-                else
-                {
-                    Console.WriteLine("Файл не выбран!");
-                    s = "";
-
-                }
-
+                ofDlg.InitialDirectory = @"D:\";
+                ofDlg.Title = "Выбор файла с числом N";
+                isFileChoose = (ofDlg.ShowDialog() == DialogResult.OK) ? true : false;
+                s = ofDlg.FileName;
             }
             return s;
         }
---
+++ Task_1/UtilityClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_05
+{
+    public class UtilityClass
+    {
+        public static void PrintHeader(string name)
+        {
+            Console.SetCursorPosition(Console.WindowWidth / 2 - name.Length, 0);
+            Console.WriteLine(name);
+        }
+       
+    }
+}
